2000f8d4f1a70d1c4ac638b2cb4f145017334984
mosheline <moshe.felixg@gmail.com>
2024-07-19 04:28:51 -0500

    Modificando Pantalla Navegación y Menu lateral para que no estén detrás de las barras superior e inferior del teléfono

--- app/src/main/java/com/example/applepsac/PantallaPrincipal.kt
+++ app/src/main/java/com/example/applepsac/PantallaPrincipal.kt
@@ -2,40 +2,17 @@ package com.example.applepsac
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Place
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,9 +21,11 @@ import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.ui.graphics.toArgb
+import androidx.core.view.WindowCompat
 
 
 @Composable
@@ -54,21 +33,31 @@ fun PantallaPrincipal(navController: NavHostController, onExitClick: () -> Unit,
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
 
+    SetSystemBarsColor(color = Color(0xFF6200EE))
+
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = { TopBar(scope, scaffoldState) },
-        drawerContent = { DrawerContent(navController,onExitClick) },
+        drawerContent = { DrawerContent(navController, onExitClick) },
         bottomBar = { BottomNavigationBar() }
     ) { paddingValues ->
-        Column(
+        Box(
             modifier = Modifier
+                .background(Color.White)
                 .padding(paddingValues)
                 .padding(bottom = 16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
         ) {
-            MainContent(nombreCliente)
+            Column(
+                modifier = Modifier
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MainContent(nombreCliente)
+            }
         }
     }
 }
@@ -87,41 +76,47 @@ fun TopBar(scope: CoroutineScope, scaffoldState: ScaffoldState) {
             }) {
                 Icon(Icons.Default.Menu, contentDescription = "Menu")
             }
-        }
+        },
+        modifier = Modifier.statusBarsPadding()
     )
 }
 
-@Composable
-fun MyApp() {
-    val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "main") {
-        composable("main") { PantallaPrincipal(navController = navController, onExitClick = {}, nombreCliente = "Cliente") }
-        composable("orders") { OrdersApp(navController = navController) }
-    }
-}
 
 @Composable
 fun DrawerContent(navController: NavHostController, onExitClick: () -> Unit) {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+            .statusBarsPadding()
     ) {
-        Text(
-            text = "Menú",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
+        Column(
             modifier = Modifier
-                .padding(vertical = 16.dp)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        DrawerItem(icon = Icons.Default.Home, text = "Inicio")
-        DrawerItem(icon = Icons.Default.Place, text = "Seguimiento de Pedido", onClick = { navController.navigate("orders") })
-        DrawerItem(icon = Icons.Default.Settings, text = "Configuraciones")
-        DrawerItem(icon = Icons.Default.Settings, text = "Detalles del Pedido")
-        Spacer(modifier = Modifier.weight(1f))
-        DrawerItem(icon = Icons.Default.Close, text = "Salir", onClick = onExitClick)
+                .fillMaxHeight()
+                .padding(top = 24.dp, start = 16.dp, end = 16.dp)
+        ) {
+            Text(
+                text = "Menú",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(vertical = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            DrawerItem(icon = Icons.Default.Home, text = "Inicio")
+            DrawerItem(icon = Icons.Default.Place, text = "Seguimiento de Pedido", onClick = { navController.navigate("orders") })
+            DrawerItem(icon = Icons.Default.Settings, text = "Configuraciones")
+            DrawerItem(icon = Icons.Default.Settings, text = "Detalles del Pedido")
+            DrawerItem(icon = Icons.Default.Settings, text = "Detalles del Pedido")
+            Spacer(modifier = Modifier.weight(1f))
+        }
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(start = 16.dp, bottom = 48.dp, end = 16.dp) // Añadido padding en la parte inferior
+        ) {
+            DrawerItem(icon = Icons.Default.Close, text = "Salir", onClick = onExitClick)
+        }
     }
 }
 
@@ -150,11 +145,13 @@ fun DrawerItem(icon: ImageVector, text: String, onClick: (() -> Unit)? = null) {
     }
 }
 
+
 @Composable
 fun BottomNavigationBar() {
     BottomNavigation(
         backgroundColor = Color(0xFF6200EE),
-        contentColor = Color.White
+        contentColor = Color.White,
+        modifier = Modifier.navigationBarsPadding()
     ) {
         BottomNavigationItem(
             icon = { Icon(Icons.Default.Home, contentDescription = "Home") },
@@ -169,10 +166,10 @@ fun BottomNavigationBar() {
             onClick = { /* Acción cuando se selecciona Settings */ }
         )
         BottomNavigationItem(
-            icon = { Icon(Icons.Default.Email, contentDescription = "contactanos") },
+            icon = { Icon(Icons.Default.Email, contentDescription = "Contactanos") },
             label = { Text("Contactanos") },
             selected = false,
-            onClick = { /* Acción cuando se selecciona Settings */ }
+            onClick = { /* Acción cuando se selecciona Contactanos */ }
         )
     }
 }
@@ -180,7 +177,11 @@ fun BottomNavigationBar() {
 @Composable
 fun MainContent(nombreCliente: String) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp) // Optional extra padding for content
+            .statusBarsPadding()
+            .navigationBarsPadding(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -195,10 +196,25 @@ fun MainContent(nombreCliente: String) {
     }
 }
 
-/*@Preview(showBackground = true)
 @Composable
-fun PantallaPrincipal() {
-    androidx.compose.material3.MaterialTheme {
-        PantallaPrincipal(onExitClick = {})
+fun SetSystemBarsColor(color: Color) {
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val window = (view.context as androidx.activity.ComponentActivity).window
+        window.statusBarColor = color.toArgb()
+        window.navigationBarColor = color.toArgb()
+        val windowInsetsController = WindowCompat.getInsetsController(window, view)
+        windowInsetsController.isAppearanceLightStatusBars = false
+        windowInsetsController.isAppearanceLightNavigationBars = false
     }
-}*/
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewPantallaPrincipal() {
+    PantallaPrincipal(
+        navController = rememberNavController(),
+        onExitClick = {},
+        nombreCliente = "Cliente"
+    )
+}
