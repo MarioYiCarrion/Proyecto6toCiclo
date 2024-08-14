87e3df96bc988f4b9899a56d463e2fa699f142d1
Gherofx <pt46429444@idat.edu.pe>
2024-08-14 06:30:58 -0500

    actualizacion 2.0

--- app/src/main/java/com/example/applepsac/PantallaPrincipal.kt
+++ app/src/main/java/com/example/applepsac/PantallaPrincipal.kt
@@ -739,8 +739,8 @@ fun BestSellerItem(
 ) {
     Card(
         modifier = Modifier
-            .width(200.dp) // Tamaño fijo para el Card
-            .height(250.dp), // Tamaño fijo para el Card
+            .width(250.dp) // Aumentar el ancho del Card
+            .height(250.dp), // Mantener el mismo tamaño de altura
         shape = RoundedCornerShape(10.dp),
         elevation = 4.dp
     ) {
@@ -760,9 +760,9 @@ fun BestSellerItem(
             )
             Text(
                 text = title,
-                fontSize = 14.sp,
+                fontSize = 12.sp, // Reducir el tamaño del texto
                 color = Color.Black,
-                maxLines = 2,
+                maxLines = 2, // Aumentar el número de líneas permitidas
                 overflow = TextOverflow.Ellipsis
             )
         }
