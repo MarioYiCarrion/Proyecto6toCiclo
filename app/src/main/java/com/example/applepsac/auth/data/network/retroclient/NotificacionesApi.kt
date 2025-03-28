24a1478689064a37591a2548ce159debe95c7ffe
MarioYiCarrion <marioyicarrion@hotmail.com>
2025-03-27 23:00:43 -0500

    Actulizaciones Terminado - conexion API

--- app/src/main/java/com/example/applepsac/auth/data/network/retroclient/NotificacionesApi.kt
+++ app/src/main/java/com/example/applepsac/auth/data/network/retroclient/NotificacionesApi.kt
@@ -1,11 +1,16 @@
 package com.example.applepsac.auth.data.network.retroclient
 
-import com.example.applepsac.auth.data.network.request.Notificacion
 import com.example.applepsac.auth.data.network.response.NotificacionesResponse
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface NotificacionesApi {
     @GET("notificaciones/usuario/{id}")
     suspend fun getNotificaciones(@Path("id") userId: Int): NotificacionesResponse
+
+    @PUT("notificaciones/visto/{id}")
+    suspend fun marcarComoVisto(@Path("id") idNotificacion: Int, @Body body: Map<String, Int>): Response<Unit>
 }
